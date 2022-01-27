5c923d8015b9a6a79a86198396e7fab0c16bb783
drosenzweig2024 <89942068+drosenzweig2024@users.noreply.github.com>
2022-01-26 20:32:37 -0800

    commits

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DanielPersonalityQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           static int AskQuestion(Question)
+           {
+               return -1;
+           }
+           static int GetValidAnswer(List<string> answers)
+           {
+               return -1;
+           }
+           static string GetResult(List<int> scores, List<string> results)
+           {
+               return null;
+           }
+        }
+    }
+
+    class Question
+            {
+                string Question;
+                List<string> Answers;
+            }
+}
