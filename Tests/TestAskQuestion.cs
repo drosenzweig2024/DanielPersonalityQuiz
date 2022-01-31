d4fb22dd799b679c7515f1f37116331ed1723c38
drosenzweig2024 <89942068+drosenzweig2024@users.noreply.github.com>
2022-01-30 21:56:15 -0800

    current work

---
+++ Tests/TestAskQuestion.cs
@@ -0,0 +1,14 @@
+using System;
+using System.IO;
+
+namespace DanielPersonalityQuiz
+{
+    class TestAskQuestion
+    {
+        public static bool RunTest()
+        {
+            return false;
+        }
+    }
+
+}
---
+++ Tests/TestGetValidAnswer.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+
+namespace DanielPersonalityQuiz
+{
+    class TestGetValidAnswer
+     {
+        public static bool RunTest()
+        {
+            return false;
+        }
+    }
+}
