265941b00eb37ec384edd976caf0e8b512880f48
Joseph Collard <jcollard@users.noreply.github.com>
2022-02-04 07:13:00 -0800

    chore: Update feedback.

--- Tests/TestAskQuestion.cs
+++ Tests/TestAskQuestion.cs
@@ -8,49 +8,55 @@ namespace DanielPersonalityQuiz
     {
         public static bool RunTest()
         {
-            // TODO(jcollard 2022-02-04):
-            // 1. Start by setting up a Question object
-            // We do this because your method AskQuestion takes 1 parameter which must be a Question
-            
+            // TODO(jcollard 2022-02-04): 
+            // This method should test calling the `AskQuestion` method.
+            // The `AskQuestion` method has 1 parameter which accepts a `Question` type.
 
-            // TODO(jcollard 2022-02-03): You need to import the list library to use List.
-            // Add using `using System.Collections.Generic;` to the top of this file
-            // That said, I don't think you actually need this list here.
-            List<string> options = new List<string>();
-            Question testQuestion = new Question();
-            testQuestion.question = "Some question here";
-            testQuestion.answers.Add("Some answer");
-            testQuestion.answers.Add("Some other answer");
+            // 1. Start by creating a Question 
             
-            // Then, create a message to be displayed to the tester. What should the method
-            // write to the console when you pass testQuestion as an argument?
-            //
+            Question question = new Question();
             
-            Console.WriteLine("Expected: the output to contain 3 options.");
-            Console.WriteLine("First type 4. This should be an invalid option. It will ask again.");
-            Console.WriteLine("Next type 1. This should be a valid option. By selecting 1, the method should return 1.");
-            
-            // TODO(jcollard 2022-02-03): The error here is saying it cannot find the `AskQuestion` method. You need to do
-            // 2 things to fix this:
-            //
-            // 1. In Program.cs you need to add `public` in front of the AskQuestion method. In this case you need to change
-            //    it to `public static int AskQuestion(Question q)`. The keyword public specifies that you would like that method
-            //    to be accessible in other files
-            //
-            // 2. The AskQuestion method expects to be passed a `Question`. `options` is a `List<string>`. Instead, you want
-            //    to change this to `AskQuestion(testQuestion)`. The type of testQuesiton is `Question` which is what AskQuestion
-            //    expects.
-            int result = Program.AskQuestion(options);
-            
-            // Then we check that the result is equal to 1 because that is what we told the tester to enter.
-            // 
-            // Think of a few more tests you could do. What should happen if the question has no answers?
-            // What should happen if the question has more than 3 answers?
-            
-            Console.WriteLine($"The method returned {result}.");
-            Console.WriteLine("Does this method appear to be working?");
-            
-            return false;
+            // 2. Set the question
+            question.question = "What is your favorite food?";
+
+            // 3. Add some answers to the question
+            question.answers.Add("Chicken"); // Index 0
+            question.answers.Add("Fish");    // Index 1
+
+            // 4. Display a message to the tester explaining what they should.
+            Console.WriteLine("Calling AskQuestion. Expecting to ask 'What is your favorite food?'");
+
+            // 5. Tell the tester to etner some invalid options.
+            Console.WriteLine("You should see 2 options. Try an invalid option by typing 3. This should display 'Invalid' and prompt again.");
+
+            // 6. Tell the test to enter a valid input
+            Console.WriteLine("Then select a valid option: 2. This should be valid");
+
+            // 7. Finally, call the method
+            int result = Program.AskQuestion(question);
+
+            // 8. Assume the tester will follow the instructions. What should the expected result of the method be?
+            //    In this case, it should be 1 (e.g. Fish which is at index 1 of the list)
+            int expected = 1;
+
+            // 9. Check to see if the result does not match what is expected
+            if (result != expected)
+            {
+                // 10. IF the result does not match, we tell the tester what went wrong:
+                Console.Error.WriteLine($"Expected the result to be {expected} but was {result}.");
+
+                // 11. We return false to fail the test
+                return false;
+            }
+
+            // TODO: Write at least 2 more test cases
+            // 1. Test with a list that has more than 3 elements.
+            // 2. What are some additional invalid inputs to test?
+            // 3. Make sure to test at least 2 more valid inputs
+
+
+            // TODO(jcollard 2022-02-04): If we make it this far, the test is passing so we return true
+            return true;
         }
     }
 
