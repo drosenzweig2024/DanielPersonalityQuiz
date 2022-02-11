fb4c754125672cbfe5eede2a39b2e0686d01e3fc
drosenzweig2024 <89942068+drosenzweig2024@users.noreply.github.com>
2022-02-11 10:25:03 -0800

    logs

--- Program.cs
+++ Program.cs
@@ -5,16 +5,16 @@ namespace DanielPersonalityQuiz
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            
-            if(args.Length > 0 && args[0] == "test")
+
+            if (args.Length > 0 && args[0] == "test")
             {
                 TestAll();
                 return;
             }
-            
-            
+
+
             // Feedback(jcollard 2022-01-28): Daniel, you're off to a great
             // start! I've gone ahead and done a few corrections as well as
             // added a handful of TODO comments. You sould complete the TODO
@@ -33,7 +33,7 @@ namespace DanielPersonalityQuiz
             woodChuckQuestion.answers.Add("As much wood as a woodchuck could chuck if a woodchuck could chuck wood.");
             woodChuckQuestion.answers.Add("Uh... 14?");
             woodChuckQuestion.answers.Add("None. Clearly the woodchuck is a manifestation of all your fears.");
-            
+
             Question foodQuestion = new Question();
 
             // Next, set the question to be an actual question.
@@ -71,9 +71,9 @@ namespace DanielPersonalityQuiz
             // 4. Create a List of your results
             // 5. Call GetResults(score, results)
             // 6. Display the results
-            
-        } 
-        
+
+        }
+
         public static void TestAll()
         {
             bool testAskQuestion = TestAskQuestion.RunTest();
@@ -82,23 +82,23 @@ namespace DanielPersonalityQuiz
             bool testGetResult = TestGetResult.RunTest();
             Console.WriteLine($"test GetResult(filename): {testGetResult}");
 
-             bool testGetValidAnswer = TestGetValidAnswer.RunTest();
+            bool testGetValidAnswer = TestGetValidAnswer.RunTest();
             Console.WriteLine($"test GetValidAnswer(filename): {testGetValidAnswer}");
         }
-        
-        
-        
-        
+
+
+
+
         // I added this curly bracket to end your Main method. Your other methods should be inside the Program class but not inside of this method.
-            
-            //Display the question to the user
-            //Loop through each answer and display it
-            //Using the GetValidAnswer method get and return the users response
-            /// <summary>
-            /// This displays the question and all of the answers and prompts the user to choose one the answers and returns the answer.
-            /// </summary>
-            /// <param name="question">The question that is being asked</param>
-            /// <returns>It returns the answer to the player</returns>
+
+        //Display the question to the user
+        //Loop through each answer and display it
+        //Using the GetValidAnswer method get and return the users response
+        /// <summary>
+        /// This displays the question and all of the answers and prompts the user to choose one the answers and returns the answer.
+        /// </summary>
+        /// <param name="question">The question that is being asked</param>
+        /// <returns>It returns the answer to the player</returns>
         public static int AskQuestion(Question question) // I added the variable question here.
         {
             // Feedback(jcollard 2022-02-09): Do this method second
@@ -106,134 +106,138 @@ namespace DanielPersonalityQuiz
             // Here is an exmaple showing how to iterate through a list of
             // options and display them to the console: 
             // https://jcollard.github.io/IntroToCSharpSite/examples/display-options
-            
+
             // TODO(jcollard 2022-02-11): You cannot define a method inside of a
             // method
-            // DELETE BELOW THIS LINE
 
-            static void DisplayOptions(List<string> options) 
+            // TODO(jcollard 2022-02-11): You should modify this code to use
+            // your question and quesiton.answers. This will not work as is
+            if (question.answers == null) throw new ArguementNullException("List of answers may not be null");
+            if (question.answers == 0) throw new ArgumentException("There should be at least 1 answer in the list");
+            int index = 1;
+            foreach (string answers in question.answers)
             {
-
-            // DELETE ABOVE THIS LINE
-
-                // TODO(jcollard 2022-02-11): You should modify this code to use
-                // your question and quesiton.answers. This will not work as is
-                if(options == null) throw new ArguementNullException("List of options may not be null");
-                if(options.Count == 0) throw new ArgumentException("There should be at least 1 option in the list");
-                int index = 1;
-                foreach (string option in options)
-                {
-                    Console.WriteLine($"{index}. {option}");
-                    index = index + 1;
-                }
-
-                // TODO(jcollard 2022-02-11): After you display the options, you
-                // need to get a valid answer. This can be done by calling
-                // GetValidAnswer and pass in question.answers.
-                // You should then return the result of calling that method
+                Console.WriteLine($"{index}. {answers}");
+                index = index + 1;
 
             }
-            // DELETE BELOW THIS LINE
-            static void Main()
-            {
-                List<string> options = new List<string>();
-                options.Add("First Choice");
-                options.Add("Second Choice");
-                options.Add("Third Choice");
-                DisplayOptions(options);
-            }
-            // DELETE ABOVE THIS LINE
+            // TODO(jcollard 2022-02-11): After you display the options, you
+            // need to get a valid answer. This can be done by calling
+            // GetValidAnswer and pass in question.answers.
+            // You should then return the result of calling that method
+            int userChoice = GetValidAnswer(question.answers);
 
-            return -1; 
-        }
 
-            //Validate that there is at least 1 possible answer.
-            //If the list of answers is empty then throw an exception
-            //If not display a message that asks the user to pick an option
-            //Store the user's response in a variable name choice
-            //Validate that the users choice is one of the possible answers
-            //If it is not a valid answer then display an invalid command message
-            //Otherwise, return the user's choice.
-            /// <summary>
-            /// Given a list of answers, prompts the user to enter a number corresponding to one of the answers. 
-            /// If the user enters a number that is not within the specified range,
-            ///  the user is asked to enter another value.
-            /// </summary>
-            ///<param name="answers">The answer that is being validated in the method</param>
-            /// <returns>It returns the user's choice or an invalid command message</returns>
-        public static int GetValidAnswer(List<string> answers)
-        { 
-            
-            // Feedback(jcollard 2022-02-09): Do this method first.
-            
-            // Here is an example of reading input:
-            // https://jcollard.github.io/IntroToCSharpSite/examples/read-input
-            
-            // In your situation, you want to ensure that the users input is
-            // between 1 and answers.Count
-
-            // Note: This method shoudl simply ask the user to enter a value but
-            // doesn't display the actual options. You will do that in AskQuestion.
-            int userChoice;
-
-            do
-            {
-                Console.Write("Enter a number that is between 0 and 5:");
-                string input = Console.ReadLine();
-                bool isANumber = int.TryParse(input, out userChoice);
-                if (isANumber == false)
-                {
-                    Console.Error.WriteLine("You did not enter a number from 0 to 5");
-                }
-                else if (userChoice < 0)
-                {
-                    Console.WriteLine("That number is not greater than 0");
-                }
-                else if (userChoice > answers.Count)
-                {
-                    Console.WriteLine("That number is not less than 5");
-                }
-
-            } while (userChoice < 0 || userChoice > answers.Count);
             return userChoice;
         }
 
-            //Initialize highest to 0 which shows the highest score we have gotten.
-            //Initialize highestIx to 0 which shows the highest score we have gotten.
-            //Loop through each score in scores, tracking the index in a variable currentIx
-            //If the score is greater than the highest score then update the highest to store the new score
-            //Update highestIx to store the currentIx 
-            //After we have checked each score, highestIx should be the index of the highest score.
-            //return results[highestIx]
-            /// <summary>
-            /// Given two lists, one representing the scores for each possible 
-            /// result and one representing each possible result, 
-            /// determines which score is the highest and returns the corresponding string from the results list.
-            /// </summary>
-            /// <param name="scores"> The amount of points for each result</param>
-            /// <param name="results">A list of results the player could get</param>
-            /// <returns>It returns the corresponding string from the results list</returns>
-        public static string GetResult(List<int> scores, List<string> results)
-        {
-            // TODO(jcollard 2022-02-11):
-            // Complete this method last. I've created an example
-            // showing how you can use two lists that are associated to
-            // find the shortest person in a list.
-            //
-            // Tips:
-            // * In the example, they use a list of doubles. Your scores are int values.
-            // * You are looking for the highest score rather than the lowest height. 
-            // * The names of your lists are scores (rather than heights) and results (rather than names).
-            //
-            // Start by copying everything inside of the method here first.
-            // Then modify it to meet your needs:
-            // https://jcollard.github.io/IntroToCSharpSite/examples/association-list
-           
-            return null;
-        }
+
+        // put methods here
         
+
     }
 
+    //Validate that there is at least 1 possible answer.
+    //If the list of answers is empty then throw an exception
+    //If not display a message that asks the user to pick an option
+    //Store the user's response in a variable name choice
+    //Validate that the users choice is one of the possible answers
+    //If it is not a valid answer then display an invalid command message
+    //Otherwise, return the user's choice.
+    /// <summary>
+    /// Given a list of answers, prompts the user to enter a number corresponding to one of the answers. 
+    /// If the user enters a number that is not within the specified range,
+    ///  the user is asked to enter another value.
+    /// </summary>
+    ///<param name="answers">The answer that is being validated in the method</param>
+    /// <returns>It returns the user's choice or an invalid command message</returns>
+    public static int GetValidAnswer(List<string> answers)
+    {
+
+        // Feedback(jcollard 2022-02-09): Do this method first.
+
+        // Here is an example of reading input:
+        // https://jcollard.github.io/IntroToCSharpSite/examples/read-input
+
+        // In your situation, you want to ensure that the users input is
+        // between 1 and answers.Count
+
+        // Note: This method shoudl simply ask the user to enter a value but
+        // doesn't display the actual options. You will do that in AskQuestion.
+        int userChoice;
+
+        do
+        {
+            Console.Write("Enter a number that is between 0 and 5:");
+            string input = Console.ReadLine();
+            bool isANumber = int.TryParse(input, out userChoice);
+            if (isANumber == false)
+            {
+                Console.Error.WriteLine("You did not enter a number from 0 to 5");
+            }
+            else if (userChoice < 0)
+            {
+                Console.WriteLine("That number is not greater than 0");
+            }
+            else if (userChoice > answers.Count)
+            {
+                Console.WriteLine("That number is not less than 5");
+            }
+
+        } while (1 < answers.Count);
+        return userChoice;
+    }
+
+    //Initialize highest to 0 which shows the highest score we have gotten.
+    //Initialize highestIx to 0 which shows the highest score we have gotten.
+    //Loop through each score in scores, tracking the index in a variable currentIx
+    //If the score is greater than the highest score then update the highest to store the new score
+    //Update highestIx to store the currentIx 
+    //After we have checked each score, highestIx should be the index of the highest score.
+    //return results[highestIx]
+    /// <summary>
+    /// Given two lists, one representing the scores for each possible 
+    /// result and one representing each possible result, 
+    /// determines which score is the highest and returns the corresponding string from the results list.
+    /// </summary>
+    /// <param name="scores"> The amount of points for each result</param>
+    /// <param name="results">A list of results the player could get</param>
+    /// <returns>It returns the corresponding string from the results list</returns>
+    public static string GetResult(List<int> scores, List<string> results)
+    {
+        // TODO(jcollard 2022-02-11):
+        // Complete this method last. I've created an example
+        // showing how you can use two lists that are associated to
+        // find the shortest person in a list.
+        //
+        // Tips:
+        // * In the example, they use a list of doubles. Your scores are int values.
+        // * You are looking for the highest score rather than the lowest height. 
+        // * The names of your lists are scores (rather than heights) and results (rather than names).
+        //
+        // Start by copying everything inside of the method here first.
+        // Then modify it to meet your needs:
+        // https://jcollard.github.io/IntroToCSharpSite/examples/association-list
+        if (result == null || scores == null) throw new Exception("must be non-null.");
+        if (results.Count == 0) throw new Exception("Cannot process an empty list");
+        if (scores.Count != results.Count) throw new Exception("Results and scores were not the same length");
+
+        int highestScore = score[0];
+        string highestResult = result[0];
+
+        int index1 = 0;
+        foreach (int score in scores)
+        {
+            if (score > highestScore)
+            {
+                highestScore = score[index];
+                highestResult = results[index];
+            }
+            index1 = index + 1;
+        }
+
+        return highestResult;
+    }
     class Question
     {
         // Feedback(jcollard 2022-01-28): I modified this class by making the
@@ -243,7 +247,7 @@ namespace DanielPersonalityQuiz
         //
         // Note: Marking the field as public allows it to be used in your
         // Program class.
-        public string question; 
+        public string question;
 
         // Feedback(jcollard 2022-01-28): This constructs a new list so we
         // can add answers to it.
@@ -251,3 +255,6 @@ namespace DanielPersonalityQuiz
 
     }
 }
+
+
+
