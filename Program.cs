fd76db157c48bb51b92a0a7a42b0e0475ff1a211
drosenzweig2024 <89942068+drosenzweig2024@users.noreply.github.com>
2022-02-11 17:49:20 -0800

    work

--- Program.cs
+++ Program.cs
@@ -132,129 +132,101 @@ namespace DanielPersonalityQuiz
         }
 
 
-        // put methods here
-        
-
-    }
-
-    //Validate that there is at least 1 possible answer.
-    //If the list of answers is empty then throw an exception
-    //If not display a message that asks the user to pick an option
-    //Store the user's response in a variable name choice
-    //Validate that the users choice is one of the possible answers
-    //If it is not a valid answer then display an invalid command message
-    //Otherwise, return the user's choice.
-    /// <summary>
-    /// Given a list of answers, prompts the user to enter a number corresponding to one of the answers. 
-    /// If the user enters a number that is not within the specified range,
-    ///  the user is asked to enter another value.
-    /// </summary>
-    ///<param name="answers">The answer that is being validated in the method</param>
-    /// <returns>It returns the user's choice or an invalid command message</returns>
-    public static int GetValidAnswer(List<string> answers)
-    {
-
-        // Feedback(jcollard 2022-02-09): Do this method first.
-
-        // Here is an example of reading input:
-        // https://jcollard.github.io/IntroToCSharpSite/examples/read-input
-
-        // In your situation, you want to ensure that the users input is
-        // between 1 and answers.Count
-
-        // Note: This method shoudl simply ask the user to enter a value but
-        // doesn't display the actual options. You will do that in AskQuestion.
-        int userChoice;
-
-        do
+        //Validate that there is at least 1 possible answer.
+        //If the list of answers is empty then throw an exception
+        //If not display a message that asks the user to pick an option
+        //Store the user's response in a variable name choice
+        //Validate that the users choice is one of the possible answers
+        //If it is not a valid answer then display an invalid command message
+        //Otherwise, return the user's choice.
+        /// <summary>
+        /// Given a list of answers, prompts the user to enter a number corresponding to one of the answers. 
+        /// If the user enters a number that is not within the specified range,
+        ///  the user is asked to enter another value.
+        /// </summary>
+        ///<param name="answers">The answer that is being validated in the method</param>
+        /// <returns>It returns the user's choice or an invalid command message</returns>
+        public static int GetValidAnswer(List<string> answers)
         {
-            Console.Write("Enter a number that is between 0 and 5:");
-            string input = Console.ReadLine();
-            bool isANumber = int.TryParse(input, out userChoice);
-            if (isANumber == false)
-            {
-                Console.Error.WriteLine("You did not enter a number from 0 to 5");
-            }
-            else if (userChoice < 0)
-            {
-                Console.WriteLine("That number is not greater than 0");
-            }
-            else if (userChoice > answers.Count)
-            {
-                Console.WriteLine("That number is not less than 5");
-            }
 
-        } while (1 < answers.Count);
-        return userChoice;
-    }
+            int userChoice;
 
-    //Initialize highest to 0 which shows the highest score we have gotten.
-    //Initialize highestIx to 0 which shows the highest score we have gotten.
-    //Loop through each score in scores, tracking the index in a variable currentIx
-    //If the score is greater than the highest score then update the highest to store the new score
-    //Update highestIx to store the currentIx 
-    //After we have checked each score, highestIx should be the index of the highest score.
-    //return results[highestIx]
-    /// <summary>
-    /// Given two lists, one representing the scores for each possible 
-    /// result and one representing each possible result, 
-    /// determines which score is the highest and returns the corresponding string from the results list.
-    /// </summary>
-    /// <param name="scores"> The amount of points for each result</param>
-    /// <param name="results">A list of results the player could get</param>
-    /// <returns>It returns the corresponding string from the results list</returns>
-    public static string GetResult(List<int> scores, List<string> results)
-    {
-        // TODO(jcollard 2022-02-11):
-        // Complete this method last. I've created an example
-        // showing how you can use two lists that are associated to
-        // find the shortest person in a list.
-        //
-        // Tips:
-        // * In the example, they use a list of doubles. Your scores are int values.
-        // * You are looking for the highest score rather than the lowest height. 
-        // * The names of your lists are scores (rather than heights) and results (rather than names).
-        //
-        // Start by copying everything inside of the method here first.
-        // Then modify it to meet your needs:
-        // https://jcollard.github.io/IntroToCSharpSite/examples/association-list
-        if (result == null || scores == null) throw new Exception("must be non-null.");
-        if (results.Count == 0) throw new Exception("Cannot process an empty list");
-        if (scores.Count != results.Count) throw new Exception("Results and scores were not the same length");
-
-        int highestScore = score[0];
-        string highestResult = result[0];
-
-        int index1 = 0;
-        foreach (int score in scores)
-        {
-            if (score > highestScore)
+            do
             {
-                highestScore = score[index];
-                highestResult = results[index];
-            }
-            index1 = index + 1;
+                Console.Write("Enter a number that is between 0 and 5:");
+                string input = Console.ReadLine();
+                bool isANumber = int.TryParse(input, out userChoice);
+                if (isANumber == false)
+                {
+                    Console.Error.WriteLine("You did not enter a number from 0 to 5");
+                }
+                else if (userChoice < 0)
+                {
+                    Console.WriteLine("That number is not greater than 0");
+                }
+                else if (userChoice > answers.Count)
+                {
+                    Console.WriteLine("That number is not less than 5");
+                }
+
+            } while (1 < answers.Count);
+            return userChoice;
         }
 
-        return highestResult;
-    }
-    class Question
-    {
-        // Feedback(jcollard 2022-01-28): I modified this class by making the
-        // fields public and changing the parameter names to lower case. There
-        // was an error stating that the variable name Question had to be
-        // different than the class name Question.
-        //
-        // Note: Marking the field as public allows it to be used in your
-        // Program class.
-        public string question;
+        //Initialize highest to 0 which shows the highest score we have gotten.
+        //Initialize highestIx to 0 which shows the highest score we have gotten.
+        //Loop through each score in scores, tracking the index in a variable currentIx
+        //If the score is greater than the highest score then update the highest to store the new score
+        //Update highestIx to store the currentIx 
+        //After we have checked each score, highestIx should be the index of the highest score.
+        //return results[highestIx]
+        /// <summary>
+        /// Given two lists, one representing the scores for each possible 
+        /// result and one representing each possible result, 
+        /// determines which score is the highest and returns the corresponding string from the results list.
+        /// </summary>
+        /// <param name="scores"> The amount of points for each result</param>
+        /// <param name="results">A list of results the player could get</param>
+        /// <returns>It returns the corresponding string from the results list</returns>
+        public static string GetResult(List<int> scores, List<string> results)
+        {
+            if (result == null || scores == null) throw new Exception("must be non-null.");
+            if (results.Count == 0) throw new Exception("Cannot process an empty list");
+            if (scores.Count != results.Count) throw new Exception("Results and scores were not the same length");
 
-        // Feedback(jcollard 2022-01-28): This constructs a new list so we
-        // can add answers to it.
-        public List<string> answers = new List<string>();
+            int highestScore = score[0];
+            string highestResult = result[0];
 
+            int index1 = 0;
+            foreach (int score in scores)
+            {
+                if (score > highestScore)
+                {
+                    highestScore = score[index];
+                    highestResult = results[index];
+                }
+                index1 = index + 1;
+            }
+
+            return highestResult;
+        }
+        class Question
+        {
+            // Feedback(jcollard 2022-01-28): I modified this class by making the
+            // fields public and changing the parameter names to lower case. There
+            // was an error stating that the variable name Question had to be
+            // different than the class name Question.
+            //
+            // Note: Marking the field as public allows it to be used in your
+            // Program class.
+            public string question;
+
+            // Feedback(jcollard 2022-01-28): This constructs a new list so we
+            // can add answers to it.
+            public List<string> answers = new List<string>();
+
+        }
     }
 }
 
-
-
+   
